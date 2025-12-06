359cc205103d38f7f8b304144c360e444d7d4c70
BorbelyBettina <borbelybettina01@gmail.com>
2025-12-06 16:56:55 +0100

    kezdés

---
+++ src/main/java/com/example/javabeadando/JavabeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabeadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabeadandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabeadandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javabeadando/JavabeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabeadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabeadandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
